32c5b0f01dd87139931a70ef27f2d8e91a1ff79f
alec202 <102689459+alec202@users.noreply.github.com>
2022-12-07 14:09:08 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+from document_tools import DataFile, ReportGenerator
+def main():
+    datafile = DataFile("../data/vgsales.csv", "VGSales Report")
+    datafile.set_description("This is a summary of video game sales information.")
+    datafile.load()
+    datafile.set_process([6, 7, 8, 9, 10, 11])
+    generator = ReportGenerator(datafile)
+    generator.generate('../data/output.docx')
+if __name__ == '__main__':
+    main()
