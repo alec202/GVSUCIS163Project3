5b6127eb3928f9e0cc55d47a1b8973993952b1e3
alec202 <102689459+alec202@users.noreply.github.com>
2022-12-07 14:08:27 -0500

    Create Analysis_tools.py

---
+++ Analysis_tools.py
@@ -0,0 +1,63 @@
+import math
+
+
+class MyMath:
+    @staticmethod
+    def cleanup(data):
+        float_list = []
+        for string in data:
+            if data != "":
+                float_list.append(float(string))
+
+        return float_list
+      
+    @staticmethod
+    def mean(data):
+        float_data = MyMath.cleanup(data)
+        float_sum = sum(float_data)
+        mean = float_sum / len(float_sum)
+        return mean
+
+    @staticmethod
+    def median(data):
+        float_nums = MyMath.cleanup(data)
+        float_nums.sort()
+
+        # append indices of middle most numbers
+        if len(float_nums) % 2 == 1:
+            return float_nums[len(float_nums) // 2]
+        else:
+            middle_value_sum = float_nums[len(float_nums) // 2]
+            middle_value_sum += float_nums[((len(float_nums) // 2) - 1)]
+            return middle_value_sum / 2
+
+    @staticmethod
+    def stdev(data):
+        float_nums = MyMath.cleanup(data)
+        mean = MyMath.mean(data)
+        difference_sum = 0
+        for num in float_nums:
+            difference_sum += (num - mean) ** 2
+        avg = difference_sum / len(float_nums)
+        math.sqrt(avg)
+
+    @staticmethod
+    def minimum(data):
+        float_list = MyMath.cleanup(data)
+        return min(float_list)
+
+    @staticmethod
+    def maximum(data):
+        float_list = MyMath.cleanup(data)
+        return max(float_list)
+
+
+
+
+
+
+
+
+
+
+
