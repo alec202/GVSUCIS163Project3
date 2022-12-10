4b233e2c0f7913633d52617ff685da18d2f5059f
alec202 <102689459+alec202@users.noreply.github.com>
2022-12-09 19:18:20 -0500

    Update Analysis_tools.py

--- Analysis_tools.py
+++ Analysis_tools.py
@@ -1,62 +1,96 @@
 import math
+# importing of the math module
 
-
+# Creating MyMath class
 class MyMath:
-    @staticmethod
+
+    # Function to rid of the empty spaces
     def cleanup(data):
-        float_list = []
-        for string in data:
-            if data != "":
-                float_list.append(float(string))
+        """
+        :param data: list that the empty spaces will be removed for
+        :return: the list without empty spots
+        """
+        cleanup = []
+        for x in data:
+            if x != '':
+                x = float(x)
+                cleanup.append(x)
+        return cleanup
 
-        return float_list
-      
-    @staticmethod
+     # function to calculate the mean
     def mean(data):
-        float_data = MyMath.cleanup(data)
-        float_sum = sum(float_data)
-        mean = float_sum / len(float_sum)
-        return mean
+        """
+        :param data: list that the mean will be found for.
+        :return: the calculated mean
+        """
+         newData = MyMath.cleanup(data)
+         mean = float(math.fsum(newData)/len(newData))
+         float_sum = sum(newData)
+         mean = float_sum/ len(newData)
+         return mean
 
+    # Function to calculate the median
     def median(data):
-        float_nums = MyMath.cleanup(data)
-        float_nums.sort()
+        """
+        :param data: list to calculate the median with
+        :return: the median of the list entered in the data parameter
+        """
+         newData = sorted(MyMath.cleanup(data))
+         a = int((len(newData)/2 + .5))
+         b = int((len(newData)/2 - .5))
+         if len(newData) % 2 == 0:
+             return len(newData)/2
+         float_nums = MyMath.cleanup(data)
+         float_nums.sort()
 
-        # append indices of middle most numbers
-        if len(float_nums) % 2 == 1:
-            return float_nums[len(float_nums) // 2]
-        else:
-            middle_value_sum = float_nums[len(float_nums) // 2]
-            middle_value_sum += float_nums[((len(float_nums) // 2) - 1)]
-            return middle_value_sum / 2
+         # append indices of middle most numbers
+         if len(float_nums) % 2 == 1:
+             return float_nums[len(float_nums) // 2]
+         else:
+             return ((newData[a] + newData[b])/2)
+             middle_value_sum = float_nums[len(float_nums) // 2]
+             middle_value_sum += float_nums[((len(float_nums) // 2) - 1)]
+             return middle_value_sum / 2
 
-    @staticmethod
+    # function to find the standard deviation
     def stdev(data):
-        float_nums = MyMath.cleanup(data)
+        """
+        :param data: list of the data we will find the standard deviation for
+        :return: the standard deviation of the list entered in the data parameter
+        """
+         total = 0
+        newData = MyMath.cleanup(data)
         mean = MyMath.mean(data)
-        difference_sum = 0
-        for num in float_nums:
-            difference_sum += (num - mean) ** 2
-        avg = difference_sum / len(float_nums)
-        math.sqrt(avg)
+        for x in newData:
+            number = (x - mean) ** 2
+            total = total + number
+        total = total/len(newData)
+        return math.sqrt(total)
 
-    @staticmethod
+    # funciton to determine the minimum value in a list
     def minimum(data):
-        float_list = MyMath.cleanup(data)
-        return min(float_list)
+        """
+        :param data: list of numbers that we will find the minimum value for
+        :return: the minimum value from the data list
+        """
+        newData = sorted(MyMath.cleanup(data))
+        minimum = newData[0]
+        return minimum
 
-    @staticmethod
+    # function to find the maximum value for the data list
     def maximum(data):
-        float_list = MyMath.cleanup(data)
-        return max(float_list)
+        """
+        :param data: list of numbers that we will find the maximum value for
+        :return: the maximum value in the data list
+        """
+        newData = sorted(MyMath.cleanup(data))
+        maximum = newData[-1]
+        return maximum
 
-
-
-
-
-
-
-
-
-
-
+ '''
+ list = ['12','','1', '6','','1','9']
+ print(MyMath.mean(list))
+ print(MyMath.median(list))
+ print(MyMath.maximum(list))
+ print(MyMath.minimum(list))
+ '''
